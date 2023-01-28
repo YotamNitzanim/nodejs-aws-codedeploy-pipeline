7e475b6e23758cc6c491cbc5dbcb097c1edb856e
YotamNitzanim <123062023+YotamNitzanim@users.noreply.github.com>
2023-01-28 12:44:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3530;
 
-const version = '133';
+const version = '14444';
 
 app.get('/', (req, res) => {
     // set response content    
