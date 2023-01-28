e7a7de4f7fcab9a4e92aae1adaee047fdac69a8f
EC2 Default User <ec2-user@ip-172-31-11-25.ap-northeast-1.compute.internal>
2023-01-28 11:11:24 +0000

    changin versions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const hostname = '127.0.0.1'; // Your server ip address
 const port = 3530;
 
-const version = '14444';
+const version = '1474';
 
 app.get('/', (req, res) => {
     // set response content    
